983adfea1bbdee5ebe7e78e113437b0935a11a2e
WHaoL <liangwenhao0603@163.com>
2020-10-25 20:04:15 +0800

    add

---
+++ DataStructure_Data Structures and Algorithm Analysis in C Second Edition - Mark Allen Weiss/Chapter04/2_avltree/fatal.h
@@ -0,0 +1,8 @@
+//fatal.h 
+//打印错误信息并退出
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define Error( Str )        FatalError( Str )
+#define FatalError( Str )   fprintf( stderr, "%s\n", Str ), exit( 1 )
